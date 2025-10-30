9d28967bf6e5d65b6df7aa707d4ddc57b27bbffa
lito412-ui <alvarogutierrez874@alumnos.alborfp.com>
2025-10-30 09:58:36 +0100

    Hola

--- Banco/src/main/java/com/miempresa/ejercicios/banco/Banco.java
+++ Banco/src/main/java/com/miempresa/ejercicios/banco/Banco.java
@@ -11,6 +11,6 @@ package com.miempresa.ejercicios.banco;
 public class Banco {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hellooooo World!");
     }
 }
